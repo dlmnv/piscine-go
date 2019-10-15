510818570668678121261319a167853319bfefcd
dlmnv <delmanov.askar@gmail.com>
2019-10-16 05:42:54 +0600

    q.two. ex.1

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,13 +1,16 @@
 package main
 
-import (	
+import (
+
 	"github.com/01-edu/z01"
+
 )
 func main() {
-
-	for i := 'a' ; i <= 'z' ; i++ {
+	
+	for i := 'a'; i <= 'z'; i++ {
 
 		z01.PrintRune(i)
+	
 	}
 	z01.PrintRune('\n')
 }
