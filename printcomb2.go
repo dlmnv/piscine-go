dda198ed94bc49f08f193984bab4d1b74e30d4e8
dlmnv <delmanov.askar@gmail.com>
2019-10-16 15:39:38 +0600

    im done

--- printcomb2.go
+++ printcomb2.go
@@ -8,19 +8,27 @@ func PrintComb2() {
 			for c := '0'; c <= '9'; c++ {
 				for d := '0'; d <= '9'; d++ {
 					if a == '9' && b == '8' && c == '9' && d == '9' {
+						z01.PrintRune(' ')
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(' ')
 						z01.PrintRune(c)
 						z01.PrintRune(d)
-					} else {
+					} else if a == '0' && b == '0' && c == '0' && d == '1' {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(' ')
 						z01.PrintRune(c)
 						z01.PrintRune(d)
 						z01.PrintRune(',')
+					}else {
 						z01.PrintRune(' ')
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(' ')
+						z01.PrintRune(c)
+						z01.PrintRune(d)
+						z01.PrintRune(',')
 					}
 				}
 			}
