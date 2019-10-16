7c56dcca0d0c50bbf82a0d25687a79c20fdb9af5
dlmnv <delmanov.askar@gmail.com>
2019-10-16 17:16:53 +0600

    im done

--- printcomb2.go
+++ printcomb2.go
@@ -14,6 +14,7 @@ func PrintComb2() {
 						z01.PrintRune(c)
 						z01.PrintRune(d)
 					} else {
+						if c > a {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(' ')
@@ -21,6 +22,16 @@ func PrintComb2() {
 						z01.PrintRune(d)
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
+					}
+					if c = a && d >b {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(' ')
+						z01.PrintRune(c)
+						z01.PrintRune(d)
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+						
 					}
 				}
 			}
