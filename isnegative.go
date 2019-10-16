e148d276ae814770d8c09505e3ba831172455b29
dlmnv <delmanov.askar@gmail.com>
2019-10-16 09:07:59 +0600

    dsadasdas

--- isnegative.go
+++ isnegative.go
@@ -2,12 +2,6 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func main() {
-	IsNegative(1)
-	IsNegative(0)
-	IsNegative(-1)
-}
-
 func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('T')
@@ -15,4 +9,10 @@ func IsNegative(nb int) {
 		z01.PrintRune('F')
 	}
 	z01.PrintRune('\n')
+}
+
+func main() {
+	IsNegative(1)
+	IsNegative(0)
+	IsNegative(-1)
 }
