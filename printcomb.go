4c733992bdb2ef49f4b51c5382da9ba0a6c25183
dlmnv <delmanov.askar@gmail.com>
2019-10-16 14:08:16 +0600

    nah

--- printcomb.go
+++ printcomb.go
@@ -3,9 +3,9 @@ package main
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for a := '0' ; a <= '7' ; a++ {
-		for b := '1' ; b <= '8' ; b++ {
-			for c := '2' ; c <= '9'; c++ {
+	for a := '0'; a <= '7'; a++ {
+		for b := '1'; b <= '8'; b++ {
+			for c := '2'; c <= '9'; c++ {
 				if a < b && b < c {
 					if a == '7' && b == '8' && c == '9' {
 						z01.PrintRune(a)
