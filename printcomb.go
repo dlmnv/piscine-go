0b9cb758536f27b72a43e1df1606020a85e3cbcc
dlmnv <delmanov.askar@gmail.com>
2019-10-16 13:46:06 +0600

    nah

--- printcomb.go
+++ printcomb.go
@@ -3,24 +3,30 @@ package main
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for a := 0; a <=7; a++ {
-		for b := 1; b <=8; b++ {
-			for c := 2; c <= 9; c++ {
-					if a < b; b < c {
-						z01.PrintComb("")
-					}else{
-						z01.PrintComb(",")
-					}
-					z01.PrintComb(a)
-					z01.PrintComb(b)
-					z01.PrintComb(c)
-
+	for a := '0' ; a <= '7' ; a++ {
+		for b := '1' ; b <= '8' ; b++ {
+			for c := '2' ; c <= '9' ; c++ {
+					if a < b && b < c {
+						if a == '7' ; b == '8' ; c == '9' {
+							z01.PrintRune('a')
+							z01.PrintRune('b')
+							z01.PrintRune('c')
+						} else {
+							z01.PrintRune('a')
+							z01.PrintRune('b')
+							z01.PrintRune('c')
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+							
+						}
 				}
 			}
 		}
+		z01.PrintComb('\n')
 	}
 }
 
-func main() {
+	func main() {
 	z01.PrintComb()
 }
