a43892726090735646cf1e4a095d12a8734812a1
dlmnv <delmanov.askar@gmail.com>
2019-10-16 11:00:45 +0600

    well

--- printcomb.go
+++ printcomb.go
@@ -6,7 +6,7 @@ func PrintComb() {
 	for a := 0; a <=7; a++ {
 		for b := 1; b <=8; b++ {
 			for c := 2; c <= 9; c++ {
-					if a := 7; b := 8; c := 9{
+					if a < b; b < c {
 						z01.PrintComb("")
 					}else{
 						z01.PrintComb(",")
