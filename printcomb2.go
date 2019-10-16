bcf179ea841dfc33c41a9b12c0ef34841c006dc6
dlmnv <delmanov.askar@gmail.com>
2019-10-16 17:21:01 +0600

    im done

--- printcomb2.go
+++ printcomb2.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 	for a := '0'; a <= '9'; a++ {
-		for b := '0'; b <= '8'; b++ {
+		for b := '0'; b <= '9'; b++ {
 			for c := '0'; c <= '9'; c++ {
 				for d := '0'; d <= '9'; d++ {
 					if a == '9' && b == '8' && c == '9' && d == '9' {
@@ -23,21 +23,19 @@ func PrintComb2() {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
-					if c = a && d > b {
+					if a = c && d > b {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
 						z01.PrintRune(' ')
 						z01.PrintRune(c)
 						z01.PrintRune(d)
 						z01.PrintRune(',')
-						z01.PrintRune(' ')
-						
+						z01.PrintRune(' ')	
 					}
 				}
 			}
 		}
 	}
-	z01.PrintRune('\n')
 }
 
 func main() {
