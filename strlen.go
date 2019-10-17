632ac656a2667c5b87f2299e62242e54770c4ecc
dlmnv <delmanov.askar@gmail.com>
2019-10-17 20:44:23 +0600

    q.3 ex.6

---
+++ strlen.go
@@ -0,0 +1,17 @@
+package main
+
+/*import "fmt"
+*/
+func StrLen(str string) int {
+	z := 0
+	for range str {
+		z++
+	}
+	return z
+}
+
+/*func main() {
+	str := "Hello World!"
+	nb := StrLen(str)
+	fmt.Println(nb)
+}*/
