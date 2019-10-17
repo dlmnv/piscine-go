3882ff39c953227bbea91e994aeb2e9c3bec3834
dlmnv <delmanov.askar@gmail.com>
2019-10-17 16:10:37 +0600

    q.three. ex.1

--- pointone.go
+++ pointone.go
@@ -1,19 +1,5 @@
-package main
+package piscine
 
-import (
-	"fmt"
-	"github.com/01-edu/z01"
-)
-
-func PointOne(n *int) {
-
-	*n = *n + 1
-
-}
-
-func main() {
-
-	n := 0
-	z01.PointOne(&n)
-	fmt.Println(n)
+func PointOne(a *int) {
+	*a = *a + 1
 }
