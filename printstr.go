94b8631fb5eeeb68758a752d7a45ed64ae93c802
dlmnv <delmanov.askar@gmail.com>
2019-10-17 20:14:25 +0600

    q.3 ex.6

---
+++ printstr.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintStr(str string) {
+	for _, a := range str {
+		z01.PrintRune(a)
+	}
+	z01.PrintRune('\n')
+}
+
+func main() {
+	str := "Hello World!"
+	PrintStr(str)
+}
