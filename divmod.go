183cc163775dfb1db5ff5e3a3db6d2dc7da7efa5
dlmnv <delmanov.askar@gmail.com>
2019-10-17 17:28:20 +0600

    q.3 ex.3

--- divmod.go
+++ divmod.go
@@ -1,8 +1,22 @@
-package piscine
+package main
+
+/*import (
+	"fmt"
+)*/
 
 func DivMod( a int, b int, div *int, mod *int) {
 
-	var *div := a / b
-	var *mod := a % b
+	*div = a / b
+	*mod = a % b
 
 }
+
+/*func main() {
+	a := 13
+	b := 2
+	var div int
+	var mod int
+	DivMod(a, b, &div, &mod)
+	fmt.Println(div)
+	fmt.Println(mod)
+}*/
