54d4c01af41a29ada7829fa7db6cf7f341615ee1
dlmnv <delmanov.askar@gmail.com>
2019-10-17 22:38:37 +0600

    q.3 ex.8

--- printstr.go
+++ printstr.go
@@ -1,14 +1,23 @@
-package piscine
 
-import "github.com/01-edu/z01"
+package main
+
+// import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
-	for _, a := range str {
-		z01.PrintRune(a)
+	var x string
+	z := 0
+	for range str {
+		z++
+	}
+
+	for a := z - 1; a >= 0; a-- {
+			x += string(s[a])
+		}
+		return a
 	}
 }
 
-func main() {
-	str := "Hello World!"
-	PrintStr(str)
-}
+// func main() {
+// 	str := "Hello World!"
+// 	PrintStr(str)
+// }
