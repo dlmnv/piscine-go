e75d0ecd8de1f1fa50787b4a04d156fd51d14826
dlmnv <delmanov.askar@gmail.com>
2019-10-21 03:29:55 +0600

    q.four ex.one

---
+++ iterativefactorial.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func IterativeFactorial(nb int) int {
+	arg := 1
+
+	for x := 1; x < nb; x++ {
+		arg = arg * (x + 1)
+	}
+	return arg
+
+
+}
+
+func main() {
+	arg := 4
+	fmt.Println(IterativeFactorial(arg))
+}
