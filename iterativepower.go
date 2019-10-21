d38263b34c3d145786fbbd975378ed99c726caf9
dlmnv <delmanov.askar@gmail.com>
2019-10-21 14:12:35 +0600

    q.four ex.three

--- iterativepower.go
+++ iterativepower.go
@@ -1,9 +1,23 @@
-package piscine
+package main
+
+import "fmt"
 
 func IterativePower(nb int, power int) int {
-	a := 1
-	for x := 1; x <= power; x++ {
-		a = a * nb
+	if power > 0 {
+		x := 1
+		for a := 1; a <= power; a++ {
+			x = x * nb
+		}
+		return x
+	} else if power == 0 {
+		return 1
+	} else {
+		return 0
 	}
-	return a
+}
+
+func main() {
+	arg1 := 4
+	arg2 := 3
+	fmt.Println(IterativePower(arg1, arg2))
 }
