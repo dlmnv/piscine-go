b6ac3851b011463570142529957e0c3d64fb3360
dlmnv <delmanov.askar@gmail.com>
2019-10-21 06:53:31 +0600

    q.four ex.three

--- iterativepower.go
+++ iterativepower.go
@@ -3,14 +3,8 @@ package piscine
 func IterativePower(nb int, power int) int {
 	a := 1
 
-	for x := 1; x <= power; x ++ {
+	for x := 1; x <= power; x++ {
 		a = a * nb 
 	}
 	return a
-}
-
-func main() {
-	arg1 := 4
-	arg2 := 3
-	fmt.Println(IterativePower(arg1, arg2))
 }
