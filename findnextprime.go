2ba65add6f79a372ea83831b93299a7afb594e5e
dlmnv <delmanov.askar@gmail.com>
2019-10-21 19:36:36 +0600

    q.four ex.eight

--- findnextprime.go
+++ findnextprime.go
@@ -1,11 +1,11 @@
 package piscine
 
-func FindNextLine(nb int) int {
+func FindNextPrime(nb int) int {
 	for x := 2; x <= nb; x++ {
 		if nb%x != 0 {
 			return nb
 		} else {
-			return FindNextLine(nb + 1)
+			return FindNextPrime(nb + 1)
 		}
 	}
 	return nb
