adb51f8386dd015c19dc39039f698a67ea81d865
dlmnv <delmanov.askar@gmail.com>
2019-10-21 06:39:39 +0600

    q.four ex.two

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,8 +1,11 @@
-package piscine
+package main
 
 func RecursiveFactorial(nb int) int {
-	if nb <= 1 {
-		return 1
+	if nb >= 0 && nb <= 20{
+		if nb <= 1 {
+			return 1
+		}
+		return nb * RecursiveFactorial(nb-1)
 	}
-	return nb * RecursiveFactorial(nb-1)
+	return 0
 }
