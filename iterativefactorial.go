32c0b86bb0668666305d8192ee6d240314e0e45b
dlmnv <delmanov.askar@gmail.com>
2019-10-21 06:16:07 +0600

    q.four ex.one

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,10 +1,20 @@
-package piscine
+package main
+
+import "fmt"
 
 func IterativeFactorial(nb int) int {
-	arg := 1
+	if nb >= 0 && nb <= 20 {
+		arg := 1
 
-	for x := 1; x < nb; x++ {
+		for x := 1; x < nb; x++ {
 		arg = arg * (x + 1)
+		}
+		return arg
 	}
-	return arg
+	return 0
+}
+
+func main() {
+	arg := 4
+	fmt.Println(IterativeFactorial(arg))
 }
