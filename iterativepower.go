5ff783b2f1e6f69b90dcdadbb5beccbbebf75601
dlmnv <delmanov.askar@gmail.com>
2019-10-21 06:58:08 +0600

    q.four ex.three

--- iterativepower.go
+++ iterativepower.go
@@ -1,19 +1,10 @@
 package main
 
-import "fmt"
-
 func IterativePower(nb int, power int) int {
 	a := 1
 
 	for x := 1; x <= power; x++ {
 		a = a * nb 
 	}
-	return a
-}
-
-
-func main() {
-	arg1 := 4
-	arg2 := 3
-	fmt.Println(IterativePower(arg1, arg2))
+	return 0
 }
