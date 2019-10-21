f006553fc9f6e0b390abd244d8be37534bcf4197
dlmnv <delmanov.askar@gmail.com>
2019-10-21 14:25:11 +0600

    q.four ex.four

--- recursivepower.go
+++ recursivepower.go
@@ -1,22 +1,14 @@
-package main
-
-// import "fmt"
+package piscine
 
 func RecursivePower(nb int, power int) int {
-	if power == 0{
+	if power == 0 {
 		return 0
 	}
 	if power == 1 {
-		return nb 
+		return nb
 	}
 	if power > 0 {
-		return nb * RecursivePower(nb, power - 1)
+		return nb * RecursivePower(nb, power-1)
 	}
 	return nb
 }
-
-// func main() {
-// 	arg1 := 4
-// 	arg2 := 3
-// 	fmt.Println(RecursivePower(arg1, arg2))
-// }
