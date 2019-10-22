8b3e3dd3aff26c83a8b1fe0dea68b78cc8c1ffbc
dlmnv <delmanov.askar@gmail.com>
2019-10-22 20:08:18 +0600

    quest.five ex.three

--- trimatoi.go
+++ trimatoi.go
@@ -5,10 +5,12 @@ func TrimAtoi(s string) int {
 	sign := 1
 
 	for _, value := range s {
-		if value >= '1' && value <= '9' {
+		if value >= '0' && value <= '9' {
 			result = result*10 + int(value-48)
 		} else if value == '-' && result == 0 {
 			sign = -1
+		} else if value == '0' && result == 0{
+			return
 		}
 	}
 
