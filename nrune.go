f42af5f6e4713de242469f4385f0caab10b83742
dlmnv <delmanov.askar@gmail.com>
2019-10-22 20:39:15 +0600

    quest.five ex.five

---
+++ nrune.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func NRune(s string, n int) rune {
+	x := []rune(s)
+	return x[n-1]
+}
+
+func main() {
+	z01.PrintRune(NRune("Hello!", 3))
+	z01.PrintRune(NRune("Salut!", 2))
+	z01.PrintRune(NRune("Ola!", 4))
+	z01.PrintRune('\n')
+}
