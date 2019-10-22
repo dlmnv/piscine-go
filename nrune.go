1e7bc445c4f5583fb4d3dafc599ccb06a1728433
dlmnv <delmanov.askar@gmail.com>
2019-10-22 20:53:06 +0600

    quest.five ex.five

--- nrune.go
+++ nrune.go
@@ -1,4 +1,6 @@
-package piscine
+package main
+
+// import "github.com/01-edu/z01"
 
 func NRune(s string, n int) rune {
 	if n > 0 && n <= slen(s) {
@@ -8,10 +10,17 @@ func NRune(s string, n int) rune {
 	return 0
 }
 
-func slen(s string) {
+func slen(s string) int {
 	k := 0
 	for range s {
 		k++
 	}
 	return k
 }
+
+// func main() {
+// 	z01.PrintRune(NRune("Hello!", 3))
+// 	z01.PrintRune(NRune("Salut!", 2))
+// 	z01.PrintRune(NRune("Ola!", 4))
+// 	z01.PrintRune('\n')
+// }
