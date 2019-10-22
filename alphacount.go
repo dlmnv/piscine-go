3b8ed121765814b873c86cb324bdabf8d7324d39
dlmnv <delmanov.askar@gmail.com>
2019-10-22 15:54:20 +0600

    quest.five ex.one

--- alphacount.go
+++ alphacount.go
@@ -1,31 +1,11 @@
 package piscine
 
-// import "fmt"
-
-func StrLen(str string) int {
-	len := 0
-	for range str {
-		len++
-	}
-	return len
-}
-
 func AlphaCount(str string) int {
-	k := []rune(str)
-	x := StrLen(str)
-	count := 0
-	for i := 0; i < x; i++ {
-		if k[i] < 91 || k[i] > 96 {
-			if k[i] >= 'A' && k[i] <= 'z' {
-				count++
-			}
+	k := 0
+	for _, c := range str {
+		if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') {
+			k++
 		}
 	}
-	return count
+	return k
 }
-
-// func main() {
-// 	str := "Hello 78 World!    4455 /"
-// 	nb := AlphaCount(str)
-// 	fmt.Println(nb)
-// }
