4fd1aee95dc145daababaef7ada0f6f9d4b81893
dlmnv <delmanov.askar@gmail.com>
2019-10-22 22:35:09 +0600

    quest.five ex.nine

---
+++ tolower.go
@@ -0,0 +1,16 @@
+package piscine
+
+func ToLower(s string) string {
+	slen := 0
+	k := []rune(s)
+	for range s {
+		slen++
+	}
+	for value := 0; value < slen; value++ {
+		if k[value] >= 'A' && k[value] <= 'Z' {
+			k[value] = (k[value] + 32)
+		}
+	}
+	upper := string(k)
+	return upper
+}
