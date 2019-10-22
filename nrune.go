d0a24e33a6b20f86ff180952a4030a8a9661f676
dlmnv <delmanov.askar@gmail.com>
2019-10-22 20:49:17 +0600

    quest.five ex.five

--- nrune.go
+++ nrune.go
@@ -1,6 +1,17 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	x := []rune(s)
-	return x[n-1]
+	if n > 0 && n <= slen {
+		x := []rune(s)
+		return x[n-1]
+	}
+	return 0
+}
+
+func slen(s string) {
+	k := 0
+	for range s {
+		k++
+	}
+	return k
 }
