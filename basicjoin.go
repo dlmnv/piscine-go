b3b7b3652062e2266fae830d4988faff89759e27
dlmnv <delmanov.askar@gmail.com>
2019-10-23 16:43:55 +0600

    quest.five ex.8teen

--- basicjoin.go
+++ basicjoin.go
@@ -1,7 +1,6 @@
 package piscine
 
 func BasicJoin(strs []string) string {
-	strs = append(strs, string(" "))
-	k := strs[0] + strs[4] + strs[1] + strs[4] + strs[2] + strs[4] + strs[3]
+	k := strs[0] + " " + strs[1] + " " + strs[2] + " " + strs[3]
 	return k
 }
