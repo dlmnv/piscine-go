b2cc71bbf9e431ca1a83d6fde63845f3cc9cac99
dlmnv <delmanov.askar@gmail.com>
2019-10-23 16:55:19 +0600

    quest.five ex.9teen

---
+++ join.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Join(strs []string) string {
+	slen := 0
+	var k string
+	for range strs {
+		slen++
+	}
+	for value := 0; value < slen; value++ {
+		k = k + strs[value]
+	}
+	return k
+}
