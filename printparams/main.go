37a2257dc781394e2fdb7268cb130588da388246
dlmnv <delmanov.askar@gmail.com>
2019-10-24 01:31:20 +0600

    quest.six ex.two

--- printparams/main.go
+++ printparams/main.go
@@ -10,7 +10,7 @@ func main() {
 	arg := os.Args
 	for k := range arg {
 		for k > 0 {
-			arr := []rune(os.Args[k])
+			arr := []rune(arg[k])
 			for a := range arr {
 				z01.PrintRune(arr[a])
 			}
