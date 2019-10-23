c4f089726dffb8d377f75d83095671bf917d3b4c
dlmnv <delmanov.askar@gmail.com>
2019-10-23 16:42:02 +0600

    8teen

--- basicjoin.go
+++ basicjoin.go
@@ -1,15 +1,7 @@
-package main
-
-import "fmt"
+package piscine
 
 func BasicJoin(strs []string) string {
 	strs = append(strs, string(" "))
 	k := strs[0] + strs[4] + strs[1] + strs[4] + strs[2] + strs[4] + strs[3]
-
 	return k
-}
-
-func main() {
-	toConcat := []string{"Hello!", "How", "are", "you?"}
-	fmt.Println(BasicJoin(toConcat))
 }
