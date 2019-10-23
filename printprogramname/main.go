11c7e3712f29f1d5db4fba1d4e509f41900d0f96
dlmnv <delmanov.askar@gmail.com>
2019-10-23 19:06:08 +0600

    quest.six ex.one

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -2,15 +2,10 @@ package piscine
 
 import (
 	"os"
-
-	"github.com/01-edu/z01"
 )
 
 func main() {
 
-	arg := []rune(os.Args[0])
-	for _, i := range arg {
-		z01.PrintRune(rune(arg[i]))
-	}
-	z01.PrintRune('\n')
+	arg := os.Args[0]
+	return arg
 }
